6c585954df7e16e39af19ee935787e0015897644
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-23 10:03:05 +0600

    AdminMiddleware grad all admin section

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/search',[HomeController::class,'search'])->name('search');
@@ -27,22 +28,21 @@ board');
 });
 
 
-Route::get('/home',[AdminController::class,'index'])->name('admin.home.page');
-Route::get('/category_page',[AdminController::class,'category_page'])->name('admin.category.page');
-Route::post('/add/category',[AdminController::class,'add_category'])->name('admin.add.category');
-Route::get('/category/delete/{id}',[AdminController::class,'Delete_category'])->name('admin.category.Delete');
-Route::get('/category/edit/{id}',[AdminController::class,'edit_category'])->name('admin.category.Edit');
-Route::post('/category/update/{id}',[AdminController::class,'update_category'])->name('admin.category.update');
+Route::get('/home',[AdminController::class,'index'])->name('admin.home.page')->middleware('admin');;
+Route::get('/category_page',[AdminController::class,'category_page'])->name('admin.category.page')->middleware('admin');;
+Route::post('/add/category',[AdminController::class,'add_category'])->name('admin.add.category')->middleware('admin');;
+Route::get('/category/delete/{id}',[AdminController::class,'Delete_category'])->name('admin.category.Delete')->middleware('admin');;
+Route::get('/category/edit/{id}',[AdminController::class,'edit_category'])->name('admin.category.Edit')->middleware('admin');;
+Route::post('/category/update/{id}',[AdminController::class,'update_category'])->name('admin.category.update')->middleware('admin');;
 
-Route::get('/add/book',[AdminController::class,'add_book'])->name('add.book');
-Route::post('/store/book',[AdminController::class,'store_book'])->name('store.book');
-Route::get('/show/book',[AdminController::class,'show_book'])->name('show.book');
-Route::get('/delete/book/{id}',[AdminController::class,'delete_book'])->name('delete.book');
-Route::get('/edit/book/{id}',[AdminController::class,'edit_book'])->name('edit.book');
-Route::post('/update/book/{id}',[AdminController::class,'update_book'])->name('update.book');
-Route::get('/borrow/request/book',[AdminController::class,'borrow_book'])->name('borrow.book.request');
-
-Route::get('/approved/book/{id}',[AdminController::class,'approve_status'])->name('approved.book.status');
-Route::get('/returned/book/{id}',[AdminController::class,'retuened_status'])->name('returned.book.status');
-Route::get('/rejected/book/{id}',[AdminController::class,'rejected_status'])->name('rejected.book.status');
+Route::get('/add/book',[AdminController::class,'add_book'])->name('add.book')->middleware('admin');
+Route::post('/store/book',[AdminController::class,'store_book'])->name('store.book')->middleware('admin');;
+Route::get('/show/book',[AdminController::class,'show_book'])->name('show.book')->middleware('admin');;
+Route::get('/delete/book/{id}',[AdminController::class,'delete_book'])->name('delete.book')->middleware('admin');;
+Route::get('/edit/book/{id}',[AdminController::class,'edit_book'])->name('edit.book')->middleware('admin');;
+Route::post('/update/book/{id}',[AdminController::class,'update_book'])->name('update.book')->middleware('admin');;
+Route::get('/borrow/request/book',[AdminController::class,'borrow_book'])->name('borrow.book.request')->middleware('admin');;
+Route::get('/approved/book/{id}',[AdminController::class,'approve_status'])->name('approved.book.status')->middleware('admin');;
+Route::get('/returned/book/{id}',[AdminController::class,'retuened_status'])->name('returned.book.status')->middleware('admin');;
+Route::get('/rejected/book/{id}',[AdminController::class,'rejected_status'])->name('rejected.book.status')->middleware('admin');;
 
